0f018a95e48659d188cd88ea641862c517ef9171
Shawn.J <shawnking07@outlook.com>
2021-10-06 16:40:52 +0800

    add example for DTO

--- src/main/java/dev/shawnking07/ecomm_system_backend/api/FileController.java
+++ src/main/java/dev/shawnking07/ecomm_system_backend/api/FileController.java
@@ -2,10 +2,8 @@ package dev.shawnking07.ecomm_system_backend.api;
 
 import dev.shawnking07.ecomm_system_backend.service.DbFileService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +18,7 @@ public class FileController {
         this.dbFileService = dbFileService;
     }
 
-    @Operation(summary = "User download files")
-    @SecurityRequirement(name = "bearerAuth")
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "Download files")
     @GetMapping("/{link}")
     public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String link) {
         return dbFileService.downloadFile(link);
--- src/main/java/dev/shawnking07/ecomm_system_backend/dto/ProductDTO.java
+++ src/main/java/dev/shawnking07/ecomm_system_backend/dto/ProductDTO.java
@@ -1,6 +1,8 @@
 package dev.shawnking07.ecomm_system_backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,16 +20,22 @@ import java.util.Set;
 @Getter
 public class ProductDTO {
     @NotBlank
+    @Schema(example = "AirPods Max")
     private String name;
+    @Schema(example = "What about Sony XM4")
     private String description;
+    @Schema(example = "499.99")
     @NotNull
     @PositiveOrZero
     private BigDecimal price;
+    @Schema(example = "459.99")
     @PositiveOrZero
     private BigDecimal discountPrice;
+    @Schema(example = "3000")
     @NotNull
     @Positive
     private Long amount = 1L;
+    @ArraySchema(uniqueItems = true, schema = @Schema(example = "Apple"))
     private Set<String> tags;
     private List<MultipartFile> files;
 }
